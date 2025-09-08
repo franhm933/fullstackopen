9ff60fd8acf0638fcc6475a494d1ad400c4b4872
Fran <franhm.93@gmail.com>
2025-09-08 11:45:11 +0200

    Exercise 3.3

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -31,7 +31,6 @@ app.get('/api/notes', (request, response) => {
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => {
-    console.log(note.id, typeof note.id, id, typeof id, note.id === id)
     return note.id === id
   })
   console.log(note)
--- part3/persons/index.js
+++ part3/persons/index.js
@@ -35,6 +35,19 @@ app.get('/api/info', (request, response) => {
     response.send('<p>Phonebook has info for ' + persons.length + ' people</p><p>'+now.toString()+'</p>')
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find( person => {
+        return person.id === id
+    })
+
+    if(person) {
+        response.json(person)
+    } else {
+        response.status('404').end()
+    }
+})
+
 
 const PORT = 3001
 app.listen(PORT)
